044bb800d112aeca0bdb7bd81ee0f3348b2e6cb0
Alexey <fier86@mail.ru>
2022-05-28 18:46:45 +0300

    Home work 2

--- src/HomWork2.java
+++ src/HomWork2.java
@@ -1,2 +1,9 @@
 public class HomWork2 {
+    public static void main(String[] args) {
+        ;
+    }
+    //1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20
+    public boolean check(int a, int b) {
+        return (10 <= a + b) && (a + b <= 20);
+    }
 }
